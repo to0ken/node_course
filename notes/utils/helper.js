ae563304ab5d3c3cbbe1c906951bd564d6a42c28
to0ken <evgeniamafinova@gmail.com>
2026-03-12 10:49:31 +0300

    Update helper.js

--- notes/utils/helper.js
+++ notes/utils/helper.js
@@ -1,1 +1,9 @@
+// скрипт помошник с дополнительными функциями для index.js
+const reindexId = (notes) =>{
 
+    return notes.map((notes, index) => ({...notes, id:index + 1})); 
+    // ... - все данные содержащиеся в notes
+    // хотим сделать все такоеже кромн id
+};
+
+module.export = {reindexId};
