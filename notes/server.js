66ee5b667d58864ef6b7753fbdb8c59c4b20c66d
to0ken <evgeniamafinova@gmail.com>
2026-03-19 12:45:53 +0300

    Update server.js

--- notes/server.js
+++ notes/server.js
@@ -1,44 +1,63 @@
-const http = require('http');
-const fs = require('fs').promises;
-const path = require('path');
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
 const helper = require("./utils/helper");
 const fileManager = require("./utils/fileManager");
 
-let notes = fileManager.loadFile
+let notes = fileManager.loadFile();
 
+const server = http.createServer(async (req, res) => {
+  const { url, method } = req;
 
-const server = http.cresteServer(async (req, res) => {
-    const {url, method} = req;
+  // ROOT ROUTERS
 
+  if (url === "/" && method === "GET") {
+    const html = await fs.readFile(path.join(__dirname, "index.html"), "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+    return;
+  }
 
-    // ROOt
+  if (url === "/app.js" && method === "GET") {
+    const js = await fs.readFile(path.join(__dirname, "app.js"), "utf-8");
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    res.end(js);
+    return;
+  }
 
-    if(url === '/' && method === 'GET'){
-        const html = await fs.readFile(path.join(__dirmame, 'index.html'), 'utf-8');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html)
+  // API ROUTERS
 
-    };
+  if (url === "/api/notes" && method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(notes));
+    return;
+  }
 
-    if (url === "/app.js" && method === "GET") {
-        const js = await fs.readFile(path.join(__dirmame, 'app.js'), 'utf-8');
-        res.writeHead(200, { "Content-Type": "application/javascript" });
-        res.end(js)
-    };
+  if (url === "/api/notes" && method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => (body += chunk));
+    req.on("end", async () => {
+      console.log("create start");
+      const { title, content } = JSON.parse(body);
+      const newNote = {
+        id: notes.length + 1,
+        title: title,
+        content: content,
+        date: new Date().toLocaleString(),
+      };
+      console.log("create end");
+      notes.push(newNote);
+      fileManager.saveFile(notes);
+      console.log(`Заметка ${newNote.title} сохранена!`);
 
-    // API 
-
-    if (url === 'api/notes' && method === 'GET'){
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(notes));
-
-    };
-
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ success: true }));
+    });
+    return;
+  }
 });
 
-server.listen(3000, () =>{
-    console.log("запущен на 3000 порв http://localhost:3000");
+server.listen(3000, () => {
+  console.log("Сервер запущен на порту http://localhost:3000");
 });
-
