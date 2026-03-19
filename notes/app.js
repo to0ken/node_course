4fdc62ba385520dc82fba8e4e23ce4aed926b34b
to0ken <evgeniamafinova@gmail.com>
2026-03-19 12:45:19 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,20 +1,32 @@
-
-const stats = document.getElementById('stats');
-const content = document.getElementById('content');
+const stats = document.getElementById("stats");
+const content = document.getElementById("content");
 
 let notes = [];
 
-async function loadNotes(){
-    try{
-        const res = await fetch('/api/notes/')
-        notes = await res.json();
-        stats.innerText = `заметок ${notes.length}`
-    }
-    catch(error){
-        console.log("Ошибка", error)
-        stats.innerText = `иныормауии нет`
+async function loadNotes() {
+  try {
+    const res = await fetch("/api/notes/");
+    notes = await res.json();
+    stats.innerText = `Заметок ${notes.length}`;
+  } catch (error) {
+    console.log("Ощибка", error);
+    stats.innerText = `Информации о заметках нет`;
+  }
+}
 
-    }
+async function addNote() {
+  const title = prompt("Введите название ");
+  const content = prompt("Введите содержание ");
+
+  try {
+    await fetch("api/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
+    });
+  } catch (error) {
+    console.log("ERROR", error.message);
+  }
 }
 
 loadNotes();
