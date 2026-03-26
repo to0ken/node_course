a3b5867a15c9fbe97e0b2ffa47d3f5dbada3ec62
to0ken <evgeniamafinova@gmail.com>
2026-03-26 12:49:59 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,13 +1,17 @@
 const stats = document.getElementById("stats");
-const content = document.getElementById("content");
+const notes_conteiner = document.getElementById("content");
 
 let notes = [];
 
 async function loadNotes() {
   try {
-    const res = await fetch("/api/notes/");
+    const res = await fetch("api/notes");
     notes = await res.json();
-    stats.innerText = `Заметок ${notes.length}`;
+    if (notes.length === 0) {
+      stats.innerText = "У вас нет заметок. Создайте свою первую заметку! \n\n";
+    } else {
+      stats.innerText = `Заметок ${notes.length}`;
+    }
   } catch (error) {
     console.log("Ощибка", error);
     stats.innerText = `Информации о заметках нет`;
@@ -17,16 +21,116 @@ async function loadNotes() {
 async function addNote() {
   const title = prompt("Введите название ");
   const content = prompt("Введите содержание ");
-
+  if (((title === null) | (content === null)) | ((!title) | (!content))) {
+    alert("Заметка не может содержать пустое название или содержание!");
+    return;
+  }
   try {
     await fetch("api/notes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title, content }),
     });
+    await showNotes();
   } catch (error) {
     console.log("ERROR", error.message);
   }
 }
 
+// редактировать заметки pad звапрс
+async function addСhange() {
+  await loadNotes();
+  if (notes.length === 0) {
+    alert("Пока нечего редактировать! Заметок нет!");
+    return;
+  }
+
+  let list = notes.map((note) => ` [${note.id}] ${note.title} `).join("\n");
+  const input = prompt(`Введите номер заметки для изменения: \n\n${list}`);
+  const id_input = parseInt(input);
+  if (!id_input) {
+    console.log("ERROR");
+    return;
+  }
+
+  // проверка на существование заметки
+  if (id_input > 0 && id_input <= notes.length) {
+    alert("Пока нечего редактировать! такой заметки нет!");
+    return;
+  }
+
+  // проходим по всему массиыу в поиске id
+  const note = notes.find((note) => note.id === id_input);
+
+  const title = prompt(`напишите новое название: `, `${note.title}`);
+  const content = prompt(`напишите новое содержание: `, `${note.content}`);
+  if (find === null) {
+    alert("ваш поиск не может быть пустым!! ");
+    return;
+  }
+  try{
+      await fetch(`api/notes/${id_input}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
+    });
+    
+  } catch (error) {
+    console.log("ERROR", error.message);
+  }
+}
+  
+
+
+async function showNotes() {
+  await loadNotes();
+  if (notes.length === 0) {
+    notes_conteiner.innerHTML = "<h2> Пока у вас нет заметок! </h2>";
+  }
+  let html = "<h2> --- Заметки --- </h2>";
+  notes.forEach((note) => {
+    html += `
+          <div style=" background-color: #b0c8ab; color: #091f14; ">
+              <small> [ ${note.id} ] ${note.date} </small>
+              <strong> ${note.title} </strong>
+              <p> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ </p>
+              <strong> ${note.content} </strong>
+          </div>
+        `;
+  });
+  notes_conteiner.innerHTML = html;
+}
+
+async function deleteNote() {
+  await loadNotes();
+  if (notes.length === 0) {
+    alert("Пока нечего удалить! Заметок нет!");
+    return;
+  }
+  let list = notes.map((note) => ` [${note.id}] ${note.title} `).join("\n");
+  const input = prompt(`Введите номер заметки для удаления: \n\n${list}`);
+
+  const id_input = parseInt(input);
+  if (!id_input) {
+    console.log("ERROR");
+    return;
+  }
+
+  if (id_input > 0 && id_input <= notes.length) {
+    const res = await fetch(`/api/notes/${id_input}`, { method: "DELETE" });
+    console.log("RES");
+    if (res.ok) {
+      await showNotes();
+    }
+  } else {
+    alert("Отмена удаления! \n Необходимо указать номер существующей заметки!");
+  }
+}
+
+
 loadNotes();
+
+window.showNotes = showNotes;
+window.addNote = addNote;
+window.deleteNote = deleteNote;
+window.addСhange = addСhange
